f632312229dc6575a52ee293446311a3b80025d5
lapox973-creator <lapox973-creator@users.noreply.github.com>
2025-11-23 18:32:09 -0500

    Add GoogleService-Info.plist and update App.swift with AppDelegate for Firebase iOS

--- iosApp/NequixiOS/App.swift
+++ iosApp/NequixiOS/App.swift
@@ -1,13 +1,18 @@
 import SwiftUI
 import FirebaseCore
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 @main
 struct NequixiOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var appState = AppState()
-    
-    init() {
-        FirebaseApp.configure()
-    }
     
     var body: some Scene {
         WindowGroup {
